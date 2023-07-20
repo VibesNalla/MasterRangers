e27976e01825fdf04ba25d49dc2bca6833dbc5ad
VibesNalla <117597116+VibesNalla@users.noreply.github.com>
2023-07-20 17:01:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img){
+        block_image_object = Img;
+
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:block_y,
+            left:block_x
+        });
+        canvas.add(block_image_object);
+    });
+}
+
+
+
+if(keyPressed == '82')
+{
+    new_image('rr1.png');
+    console.log("r");
+}
+
+window.addEventListener("keydown", my_keydown);
